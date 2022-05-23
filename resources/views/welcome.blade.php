1f623417397ae88ce6657c185a901c7751ec1094
Mohammad Yousuf <mohammadyousuf5514@gmail.com>
2022-05-23 16:00:04 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <meta name="copyright" content="MACode ID, https://macodeid.com/">
+
+  <title>Complaint Management System - Tourist Police</title>
+
+  <link rel="stylesheet" href="../assets/css/maicons.css">
+
+  <link rel="stylesheet" href="../assets/css/bootstrap.css">
+
+  <link rel="stylesheet" href="../assets/vendor/owl-carousel/css/owl.carousel.css">
+
+  <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
+
+  <link rel="stylesheet" href="../assets/css/theme.css">
+  
+</head>
+<body>
+
+  <!-- Back to top button -->
+  <div class="back-to-top"></div>
+
+  <header>
+    <div class="topbar">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 text-sm">
+            <div class="site-info">
+              <a href="#"><span class="mai-call text-primary"></span> +8801320163599 </a>
+              <span class="divider">|</span>
+              <a href="#"><span class="mai-mail text-primary"></span> digtp@police.gov.bd</a>
+            </div>
+          </div>
+          <div class="col-sm-4 text-right text-sm">
+            <div class="social-mini-button">
+              <a href="https://www.facebook.com/TouristPolice.BD/" target="_blank"><span class="mai-logo-facebook-f"></span> Facebook Page</a>
+            </div>
+          </div>
+        </div> <!-- .row -->
+      </div> <!-- .container -->
+    </div> <!-- .topbar -->
+
+    <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+      <div class="container">
+        <a class="navbar-brand" href="#"><span class="text-primary">Complaint Management System</span> - Tourist Police </a>
+
+        
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupport" aria-controls="navbarSupport" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupport">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="btn btn-primary ml-lg-3" href="{{ route('login') }}">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-primary ml-lg-3" href="{{ route('register') }}">Register</a>
+            </li>
+          </ul>
+        </div> <!-- .navbar-collapse -->
+      </div> <!-- .container -->
+    </nav>
+  </header>
+
+  <div class="page-hero bg-image overlay-dark" style="background-image: url(../assets/img/Tourist-Police-Bangladesh.jpg);">
+    <div class="hero-section">
+      <div class="container text-center wow zoomIn">
+        <span class="subhead">Here to help make your visit safer</span>
+        <h1 class="display-4">Bangladesh Tourist Police</h1>
+        <a href="{{ route('register') }}" class="btn btn-primary">Get Started</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="page-section pb-0">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6 py-3 wow fadeInUp">
+            <h1>Hello Tourists</h1>
+            <p class="text-grey mb-4">Thank you for choosing Bangladesh as your holiday destination. To ensure your safety and help you quickly and effeciently, Bangladesh Tourist Police is here at your disposal. Our contact information of the Regional Offices as well as their locations are mentioned in the following section. The 24/7 helpline number and email address is given at the top of the page. For a more detailed complaint regarding your problems simply register for an account and in the dashboard you will find a complaint form. Fill out all the necessary details and we will contact you as soon as possible. Below is a step by step breakdown of the online complaint submission process. <br>     <p style="text-align: right"> -Bangladesh Tourist Police</p>
+            </p>
+          </div>
+          <div class="col-lg-6 wow fadeInRight" data-wow-delay="400ms">
+            <div class="img-place custom-img-1">
+              <img src="../assets/img/mujibyear.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+     
+    <h2 style="text-align: center; margin-bottom: 2%; margin-top: 5%">How To Lodge Complaint, Step-by-Step Process</h2>
+    <div class="row ml-9 card-group" style="width: 80%; margin: auto">
+  <div class="col-sm-4">
+    <div class="card border-success">
+      <div class="card-body">
+        <h5 class="card-title">Step 1</h5>
+        <hr>
+        <p class="card-text">Register for a new account by clicking on the "Register" button on the top right corner of the page or on the "Get Started" button on the previous section. Enter your email address and password to complete registration.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card border-success">
+      <div class="card-body">
+        <h5 class="card-title">Step 2</h5>
+        <hr>
+        <p class="card-text">Log in to the system using your email and password. You will be redirected to a dashboard.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card border-success">
+      <div class="card-body">
+        <h5 class="card-title">Step 3</h5>
+        <hr>
+        <p class="card-text">You will see a Complaints section on the left hand side of the dashboard.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card border-success mt-2">
+      <div class="card-body">
+        <h5 class="card-title">Step 4</h5>
+        <hr>
+        <p class="card-text">Fill out the form in that page with correct information and submit the form.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card border-success mt-2">
+      <div class="card-body">
+        <h5 class="card-title">Step 5</h5>
+        <hr>
+        <p class="card-text">Wait for some time and the Tourist Police will contact you as soon as possible.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <div style="width: 70%; margin: auto" class="tab-pane fade show " id="content3" role="tabpanel" aria-labelledby="tab3">
+    <div class="container py-5 py-lg-0">
+    <h2 style="margin-top: 8%; margin-bottom: 5%">Our Regional Offices with their Contact Information</h2>
+    </div>
+                                <div class="table-responsive">
+                                    <table class="table table-alternate">
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Rank</th>
+                                            <th>Designation</th>
+                                            <th>Unit Address</th>
+                                            <th>Contact Number</th>
+                                            <th>Email</th>
+                                        </tr>
+                                                                                    <tr>
+                                                <td>1</td>
+                                                <td>Superintendent of police</td>
+                                                <td>SP(Dhaka Region)</td>
+                                                <td>তাহমিনা মঞ্জিল, বাড়ি নং-৯/৫, ব্লক-জে, বনশ্রী, ঢাকা।</td>
+                                                <td>
+                                                    <a href="tel:01320157975"><i class="fa fa-mobile"></i> 01320157975</a>
+                                                    
+                                                </td>
+                                                <td><a href="mailto:spddtp@police.gov.bd">spddtp@police.gov.bd</a></td>
+                                            </tr>
+                                                                                    <tr>
+                                                <td>2</td>
+                                                <td>Superintendent of police</td>
+                                                <td>SP Sylhet Region</td>
+                                                <td>শুভেচ্ছা-৩৬৮, শেখঘাট, থানা- কোতয়ালী, সিলেট।</td>
+                                                <td>
+                                                    <a href="tel:01320158275"><i class="fa fa-mobile"></i> 01320158275</a>
+                                                    
+                                                </td>
+                                                <td><a href="mailto:tourist.syl.sp@police.gov.bd">tourist.syl.sp@police.gov.bd</a></td>
+                                            </tr>
+                                                                                    <tr>
+                                                <td>3</td>
+                                                <td>Superintendent of police</td>
+                                                <td>SP Mymensingh Region</td>
+                                                <td>হোল্ডিং নং-৮৮/০৪, কেবি ইসমাইল রোড (পাট গুদাম সংলগ্ন), বলাশপুর, সদর,কোতয়ালী।</td>
+                                                <td>
+                                                    <a href="tel:01320158520"><i class="fa fa-mobile"></i> 01320158520</a>
+                                                    
+                                                </td>
+                                                <td><a href="mailto:tourist.sp.mym@police.gov.bd">tourist.sp.mym@police.gov.bd</a></td>
+                                            </tr>
+                                                                                    <tr>
+                                                <td>4</td>
+                                                <td>Superintendent of police</td>
+                                                <td>SP (Chattogram Region)</td>
+                                                <td>০২ নং আব্দুল মালেক সড়ক, দক্ষিন খুলশী, আ/এ, পাহাড়তলী,-৪২০২, চট্টগ্রাম।</td>
+                                                <td>
+                                                    <a href="tel:01320158750"><i class="fa fa-mobile"></i> 01320158750</a>
+                                                    
+                                                </td>
+                                                <td><a href="mailto:spcdtp@police.gov.bd">spcdtp@police.gov.bd</a></td>
+                                            </tr>
+                                                                                    <tr>
+                                                <td>5</td>
+                                                <td>Superintendent of police</td>
+                                                <td>SP (Cox&#039;s Bazar Region)</td>
+                                                <td>লাবনী পয়েন্ট, কক্সবাজার।</td>
+                                                <td>
+                                                    <a href="tel:01320158995"><i class="fa fa-mobile"></i> 01320158995</a>
+                                                    
+                                                </td>
+                                                <td><a href="mailto:tourist.cox.sp@police.gov.bd">tourist.cox.sp@police.gov.bd</a></td>
+                                            </tr>
+                                                                                    <tr>
+                                                <td>6</td>
+                                                <td>Superintendent of police</td>
+                                                <td>SP (Rangamati Region)</td>
+                                                <td>হোল্ডিং-১৬৫, ডিসি বাংলো রোড, সদর, রাঙ্গামাটি।</td>
+                                                <td>
+                                                    <a href="tel:01320159330"><i class="fa fa-mobile"></i> 01320159330</a>
+                                                    
+                                                </td>
+                                                <td><a href="mailto:tourist.hil.sp@police.gov.bd">tourist.hil.sp@police.gov.bd</a></td>
+                                            </tr>
+                                                                                    <tr>
+                                                <td>7</td>
+                                                <td>Superintendent of police</td>
+                                                <td>SP (Bandarban Region)</td>
+                                                <td>মেঘলা, জেলা পরিষদ বাংলো, বান্দরবান।</td>
+                                                <td>
+                                                    <a href="tel:01320159620"><i class="fa fa-mobile"></i> 01320159620</a>
+                                                    
+                                                </td>
+                                                <td></td>
+                                            </tr>
+                                                                                    <tr>
+                                                <td>8</td>
+                                                <td>Superintendent of police</td>
+                                                <td>SP (Khulna Region)</td>
+                                                <td>বাড়ি নং-৬৩৫, রোড নং-০৭, মুজগুন্নী, আ/এ মহাসড়ক, বয়রা, খুলনা।</td>
+                                                <td>
+                                                    <a href="tel:01320159850"><i class="fa fa-mobile"></i> 01320159850</a>
+                                                    
+                                                </td>
+                                                <td><a href="mailto:tourist.khu.sp@police.gov.bd">tourist.khu.sp@police.gov.bd</a></td>
+                                            </tr>
+                                                                                    <tr>
+                                                <td>9</td>
+                                                <td>Superintendent of police</td>
+                                                <td>SP (Barishal Region)</td>
+                                                <td>উকিলবাড়ি সড়ক, রুপাতলি, বরিশাল</td>
+                                                <td>
+                                                    <a href="tel:01320160155"><i class="fa fa-mobile"></i> 01320160155</a>
+                                                    
+                                                </td>
+                                                <td><a href="mailto:tourist.bari.sp@police.gov.bd">tourist.bari.sp@police.gov.bd</a></td>
+                                            </tr>
+                                                                                    <tr>
+                                                <td>10</td>
+                                                <td>Superintendent of police</td>
+                                                <td>SP (Rangpur Region)</td>
+                                                <td>বাড়ি নং-৭৮/১, সাতগাড়া মিস্ত্রিপাড়া, রোড নং-০৩, পোষ্ট+থানা+জেলা-রংপুুর</td>
+                                                <td>
+                                                    <a href="tel:01320160355"><i class="fa fa-mobile"></i> 01320160355</a>
+                                                    
+                                                </td>
+                                                <td><a href="mailto:tourist.rangp.sp@police.gov.bd">tourist.rangp.sp@police.gov.bd</a></td>
+                                            </tr>
+                                                                                    <tr>
+                                                <td>11</td>
+                                                <td>Superintendent of police</td>
+                                                <td>SP (Rajshahi Region)</td>
+                                                <td>হোল্ডিং-৭৫, ডাসপুকুর, থানাঃ রাজপাড়া, জেলাঃ রাজশাহী।</td>
+                                                <td>
+                                                    <a href="tel:01320160590"><i class="fa fa-mobile"></i> 01320160590</a>
+                                                    
+                                                </td>
+                                                <td><a href="mailto:tourist.rajs.sp@police.gov.bd">tourist.rajs.sp@police.gov.bd</a></td>
+                                            </tr>
+                                                                            </table>
+                                </div>
+                            </div>
+
+
+<footer class="page-footer">
+    <div class="container">
+      <div class="row px-md-3">
+        <div class="col-sm-6 col-lg-3 py-3">
+          <h5>Tourist Police</h5>
+          <ul class="footer-menu">
+            <li><a href="https://tourist.police.gov.bd/" target="_blank">About Us</a></li>
+        </div>
+        <div class="col-sm-6 col-lg-3 py-3">
+          <h5>Contact</h5>
+          <p class="footer-link mt-2">House #34, Road # 04, Block # J, Banasree, Rampura, Dhaka-1219</p>
+          <a href="#" class="footer-link">+88-02224405721</a>
+          <a href="#" class="footer-link">digtp@police.gov.bd</a>
+          
+        </div>
+        <div class="col-sm-6 col-lg-3 py-3">
+        <h5>Social Media</h5>
+          <div class="footer-sosmed mt-3">
+            <a href="https://www.facebook.com/TouristPolice.BD/" target="_blank"><span class="mai-logo-facebook-f"></span></a>
+          </div>
+      </div>
+      </div>
+
+      <hr>
+
+      <p id="copyright">Copyright &copy; 2022</p>
+    </div>
+  </footer>
+
+
+<script src="../assets/js/jquery-3.5.1.min.js"></script>
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+<script src="../assets/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+
+<script src="../assets/vendor/wow/wow.min.js"></script>
+
+<script src="../assets/js/theme.js"></script>
+  
+</body>
+</html>
